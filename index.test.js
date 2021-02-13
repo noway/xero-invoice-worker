c25e15638f6e8808cdc85fb9a02fb7928207ecee
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-02-13 22:11:33 +1300

    add "should sort events before constructing invoices" and "should create multiple invoices" tests

--- index.test.js
+++ index.test.js
@@ -112,6 +112,125 @@ const invoicesEventsCreateThenDelete = {
   ],
 };
 
+const invoicesEventsShuffled = {
+  items: [
+    {
+      id: 1,
+      type: 'INVOICE_CREATED',
+      content: {
+        invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+        invoiceNumber: 'INV-001',
+        lineItems: [
+          {
+            lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+            description: 'Supplies',
+            quantity: 2,
+            unitCost: 10.15,
+            lineItemTotalCost: 20.3,
+          },
+        ],
+        status: 'DRAFT',
+        dueDateUtc: '2020-04-30T10:00:00.000Z',
+        createdDateUtc: '2020-04-19T10:00:00.000Z',
+        updatedDateUtc: '2020-04-19T10:00:00.000Z',
+      },
+      createdDateUtc: '2020-04-19T10:00:00.000Z',
+    },
+    {
+      id: 3,
+      type: 'INVOICE_UPDATED',
+      content: {
+        invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+        invoiceNumber: 'INV-001',
+        lineItems: [
+          {
+            lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+            description: 'Supplies',
+            quantity: 40,
+            unitCost: 9.00,
+            lineItemTotalCost: 360.0,
+          },
+        ],
+        status: 'DRAFT',
+        dueDateUtc: '2020-06-30T10:00:00.000Z',
+        createdDateUtc: '2020-04-19T10:00:00.000Z',
+        updatedDateUtc: '2020-06-19T10:00:00.000Z',
+      },
+      createdDateUtc: '2020-06-19T10:00:00.000Z',
+    },
+    {
+      id: 2,
+      type: 'INVOICE_UPDATED',
+      content: {
+        invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+        invoiceNumber: 'INV-001',
+        lineItems: [
+          {
+            lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+            description: 'Supplies',
+            quantity: 20,
+            unitCost: 10.00,
+            lineItemTotalCost: 200.0,
+          },
+        ],
+        status: 'DRAFT',
+        dueDateUtc: '2020-05-30T10:00:00.000Z',
+        createdDateUtc: '2020-04-19T10:00:00.000Z',
+        updatedDateUtc: '2020-05-19T10:00:00.000Z',
+      },
+      createdDateUtc: '2020-05-19T10:00:00.000Z',
+    },
+  ],
+};
+
+const invoicesEventsCreateMultiple = {
+  items: [
+    {
+      id: 1,
+      type: 'INVOICE_CREATED',
+      content: {
+        invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+        invoiceNumber: 'INV-001',
+        lineItems: [
+          {
+            lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+            description: 'Supplies',
+            quantity: 2,
+            unitCost: 10.15,
+            lineItemTotalCost: 20.3,
+          },
+        ],
+        status: 'DRAFT',
+        dueDateUtc: '2020-04-30T10:00:00.000Z',
+        createdDateUtc: '2020-04-19T10:00:00.000Z',
+        updatedDateUtc: '2020-04-19T10:00:00.000Z',
+      },
+      createdDateUtc: '2020-04-19T10:00:00.000Z',
+    },
+    {
+      id: 2,
+      type: 'INVOICE_CREATED',
+      content: {
+        invoiceId: 'd2fc1b32-c0cb-4d07-b2a0-a8470b13e53a',
+        invoiceNumber: 'INV-002',
+        lineItems: [
+          {
+            lineItemId: '2a701a96-f4ce-4891-8000-fb83a80fecc9',
+            description: 'Services rendered',
+            quantity: 1,
+            unitCost: 300.0,
+            lineItemTotalCost: 300.0,
+          },
+        ],
+        status: 'DRAFT',
+        dueDateUtc: '2020-05-30T10:00:00.000Z',
+        createdDateUtc: '2020-05-19T10:00:00.000Z',
+        updatedDateUtc: '2020-05-19T10:00:00.000Z',
+      },
+      createdDateUtc: '2020-05-19T10:00:00.000Z',
+    },
+  ],
+};
 /** eventItemsToInvoices */
 
 // should create an invoice
@@ -154,3 +273,60 @@ assert.deepEqual(eventItemsToInvoices(invoicesEventsCreateThenUpdate.items), [{
 
 // should create and then delete invoice
 assert.deepEqual(eventItemsToInvoices(invoicesEventsCreateThenDelete.items), []);
+
+// should sort events before constructing invoices
+assert.deepEqual(eventItemsToInvoices(invoicesEventsShuffled.items), [{
+  invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+  invoiceNumber: 'INV-001',
+  lineItems: [
+    {
+      lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+      description: 'Supplies',
+      quantity: 40,
+      unitCost: 9.00,
+      lineItemTotalCost: 360.0,
+    },
+  ],
+  status: 'DRAFT',
+  dueDateUtc: '2020-06-30T10:00:00.000Z',
+  createdDateUtc: '2020-04-19T10:00:00.000Z',
+  updatedDateUtc: '2020-06-19T10:00:00.000Z',
+}]);
+
+// should create multiple invoices
+assert.deepEqual(eventItemsToInvoices(invoicesEventsCreateMultiple.items), [
+  {
+    invoiceId: '97f0821d-3517-471a-95f2-f00da84ec56e',
+    invoiceNumber: 'INV-001',
+    lineItems: [
+      {
+        lineItemId: '2686350b-2656-48a0-912d-763c06ef5c04',
+        description: 'Supplies',
+        quantity: 2,
+        unitCost: 10.15,
+        lineItemTotalCost: 20.3,
+      },
+    ],
+    status: 'DRAFT',
+    dueDateUtc: '2020-04-30T10:00:00.000Z',
+    createdDateUtc: '2020-04-19T10:00:00.000Z',
+    updatedDateUtc: '2020-04-19T10:00:00.000Z',
+  },
+  {
+    invoiceId: 'd2fc1b32-c0cb-4d07-b2a0-a8470b13e53a',
+    invoiceNumber: 'INV-002',
+    lineItems: [
+      {
+        lineItemId: '2a701a96-f4ce-4891-8000-fb83a80fecc9',
+        description: 'Services rendered',
+        quantity: 1,
+        unitCost: 300.0,
+        lineItemTotalCost: 300.0,
+      },
+    ],
+    status: 'DRAFT',
+    dueDateUtc: '2020-05-30T10:00:00.000Z',
+    createdDateUtc: '2020-05-19T10:00:00.000Z',
+    updatedDateUtc: '2020-05-19T10:00:00.000Z',
+  },
+]);
