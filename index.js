e5db5ea2e09233173d4dc93de2239a83f0d980fe
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-02-13 20:53:32 +1300

    lint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Command } = require('commander');
+
 const program = new Command();
 program.version('0.0.0');
 
-
 program
   .option('-f, --feed-url <http url>', 'HTTP url for the JSON event feed')
-  .option('-i, --invoice-dir <directory path>', 'Folder where the PDF files are stored')
+  .option('-i, --invoice-dir <directory path>', 'Folder where the PDF files are stored');
 
 program.parse(process.argv);
 
